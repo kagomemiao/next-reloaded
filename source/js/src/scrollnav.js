d4eeb12510a2b3db484130c12616b1b66d92fd32
kagomemiao <yusie@live.cn>
2018-04-10 14:34:26 +0800

    adjust navbar

--- source/js/src/scrollnav.js
+++ source/js/src/scrollnav.js
@@ -1,22 +1,22 @@
 $(window).scroll(function(){
-    var CurrHeight = $('.site-nav').height();
-    if($('.site-nav').offset().top > CurrHeight){
-        $('.site-nav').addClass('scroll-nav');
-        $('.menu').addClass('scroll-menu');
-        $('.menu-item').addClass('scroll-item');
-    }else{
-        $('.site-nav').removeClass('scroll-nav');
-        $('.menu').removeClass('scroll-menu');
-        $('.menu-item').removeClass('scroll-item');
-    }
+    var CurrHeight = $('.main').offset().top;
+        if($(this).scrollTop() > CurrHeight){
+            $('.site-nav').addClass('scroll-nav');
+            $('.menu').addClass('scroll-menu');
+            $('.menu-item').addClass('scroll-item');
+        }else{
+            $('.site-nav').removeClass('scroll-nav');
+            $('.menu').removeClass('scroll-menu');
+            $('.menu-item').removeClass('scroll-item');
+        }
 });
 (function(){
     var documentElem = $(document),
         nav = $('.scroll-nav'),
-        lastScrollTop = 329;
+        lastScrollTop = 0;
 
     documentElem.on('scroll',function(){
-        var CurrScrollTop = $(this).scrollTop()+329;
+        var CurrScrollTop = $(this).scrollTop();
         if (CurrScrollTop > lastScrollTop) nav.addClass('hidden');
         else nav.removeClass('hidden');
 
